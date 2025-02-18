580a2f23204212e66c4f838b27b2a102fb7d80b1
Mohd Ahmad <serious17org@gmail.com>
2025-02-18 10:33:16 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "jotai";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
